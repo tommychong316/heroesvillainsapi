52ed26cfedd1373f69bc5797252c869ce50a3996
tommychong316 <100431028+tommychong316@users.noreply.github.com>
2022-03-17 16:43:28 -0400

    changed database settings + created heroesvillains app

--- heroes_villains_project/settings.py
+++ heroes_villains_project/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@&zzy9w^!j^h9@877_3)81+$+1rwxv%caa6w+w3%nmobuj_zq7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,12 +71,7 @@ WSGI_APPLICATION = 'heroes_villains_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
