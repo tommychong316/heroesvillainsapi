41a9a3b72ce731a934187614587251076e3f440b
tommychong316 <100431028+tommychong316@users.noreply.github.com>
2022-03-17 16:50:25 -0400

    created Super model for supers app + registered supers and rest framework in project settings

--- supers/models.py
+++ supers/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Super(models.Model):
+    name = models.CharField(max_length=255)
+    alter_ego = models.CharField(max_length=255)
+    primary_ability = models.CharField(max_length=255)
+    secondary_ability = models.CharField(max_length=255)
+    catchphrase = models.CharField(max_length=255)
+    
