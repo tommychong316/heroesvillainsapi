04dc8978c838249e6c198df2dc349c69bf111e85
tommychong316 <100431028+tommychong316@users.noreply.github.com>
2022-03-21 11:33:55 -0400

    registed Super model with admin site + completed Post functionality for Super + updated rest framework decimal settings

--- heroes_villains_project/settings.py
+++ heroes_villains_project/settings.py
@@ -67,6 +67,11 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK ={
+    'COERCE_DECIMAL_TO_STRING': False
+    
+}
+
 WSGI_APPLICATION = 'heroes_villains_project.wsgi.application'
 
 
--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from xmlrpc.client import ResponseError
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SuperSerializer
+from .models import Super
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def supers_list(request):
+    if request.method == 'GET':
+        supers = Super.objects.all()
+        serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
