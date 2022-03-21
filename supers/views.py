d2b46e74f56adbcffff25414bec08ca8ddcb79cf
tommychong316 <100431028+tommychong316@users.noreply.github.com>
2022-03-21 12:05:45 -0400

    created delete functionality for supers view file

--- supers/views.py
+++ supers/views.py
@@ -23,7 +23,7 @@ def supers_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def super_detail(request, pk):
     super = get_object_or_404(Super, pk=pk)
     if request.method == 'GET':
@@ -34,5 +34,6 @@ def super_detail(request, pk):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-
-
+    elif request.method == 'DELETE':
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
